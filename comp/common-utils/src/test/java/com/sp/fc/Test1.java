5d522a8517fe34a35f253648ada5a1e53ab8c9ee
inderby <youk8253@gmail.com>
2023-02-09 15:51:26 +0900

    test commit

---
+++ comp/common-utils/src/test/java/com/sp/fc/Test1.java
@@ -0,0 +1,13 @@
+package com.sp.fc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+    @DisplayName("13. ")
+
+    @Test
+    void test_1(){
+        System.out.println("test");
+    }
+}
