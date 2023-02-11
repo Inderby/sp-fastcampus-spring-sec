af20b50fa348b38cbe9b71cf48ef19c41ccd2e31
inderby <youk8253@gmail.com>
2023-02-11 14:47:37 +0900

    implment multi-securityConfig

--- server/login-multi-chain/src/test/java/com/sp/fc/web/MultiChainProxyTest.java
+++ server/login-multi-chain/src/test/java/com/sp/fc/web/MultiChainProxyTest.java
@@ -1,6 +1,28 @@
 package com.sp.fc.web;
 
-public class MultiChainProxyTest {
+import com.sp.fc.web.student.Student;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MultiChainProxyTest {
+    int port;
+    TestRestTemplate testClient = new TestRestTemplate("choi", "1");
+
+    @DisplayName("1. 학생 리스트를 조회한다.")
+    @Test
+    void test_1(){
+        //choi:1로 로그인해서 학생 리스트를 내려받는다.
+        ResponseEntity<List<Student>> resp = testClient.exchange("http://localhost:" + port + "/api/teacher/students",
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {
+                });
+    }
 
 }
